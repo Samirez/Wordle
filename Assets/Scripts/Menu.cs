e6bd1fcab69847ad8f30cb18db32fd5b879a0581
Ahmadullah Naibi <ahmadullah@hotmail.dk>
2026-02-09 13:51:55 +0100

    created the basic button functionality

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,16 +1,69 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool isMenuOpen = true;
+    private Button startButton;
+    private GameObject menuUI, scoreUI, settingsUI;
+
+    public void Awake()
     {
-        
+        menuUI = GameObject.Find("MenuUI");
+        if (menuUI == null)
+        {
+            menuUI = gameObject;
+        }
+        menuUI.SetActive(isMenuOpen);
+        startButton = menuUI.transform.Find("StartButton").GetComponentInChildren<Button>();
+        startButton.onClick.AddListener(StartGame);
+
+        scoreUI = GameObject.Find("ScoreUI");
+        if (scoreUI != null)
+        {
+            scoreUI.SetActive(false);
+        }
+
+        settingsUI = GameObject.Find("SettingsUI");
+        if (settingsUI != null)
+        {
+            settingsUI.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        isMenuOpen = false;
+        menuUI.SetActive(isMenuOpen);
+    }
+
+    public void HighScores()
+    {
+        isMenuOpen = false;
+        menuUI.SetActive(isMenuOpen);
+        if (scoreUI != null)
+        {
+            scoreUI.SetActive(true);
+        }
+    }
+
+    public void OpenSettings()
+    {
+        isMenuOpen = false;
+        menuUI.SetActive(isMenuOpen);
+        if (settingsUI != null)
+        {
+            settingsUI.SetActive(true);
+        }
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
