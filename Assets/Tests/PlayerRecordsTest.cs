0039c045b8444ebed8cbe3aeef653ab80f0bd331
Ahmadullah Naibi <ahmadullah@hotmail.dk>
2026-02-13 10:53:19 +0100

    playerRecords unit test of functionality

--- Assets/Tests/PlayerRecordsTest.cs
+++ Assets/Tests/PlayerRecordsTest.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 using UnityEngine.TestTools;
 
 
-public class UnitTest
+
+public class PlayerRecordsTest
 {
+    
     // A Test behaves as an ordinary method
     [Test]
-    public void UnitTestSimplePasses()
+    public void PlayerRecordsSimplePasses()
     {
         // Use the Assert class to test conditions
+    }
+
+    [Test]
+    public bool DatabaseFileExists()
+    {
+        string filePath = Application.dataPath + "/Data_storage/PlayerRecords.db";
+        return System.IO.File.Exists(filePath);
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator UnitTestWithEnumeratorPasses()
+    public IEnumerator PlayerRecordsWithEnumeratorPasses()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
